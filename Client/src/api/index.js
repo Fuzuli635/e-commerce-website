935e6abd7d90864c428187dad3a05c22a5d0bc64
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-09-07 20:28:11 +0400

    Change baseURL

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const instance = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Authorization: "bearer " + import.meta.env.VITE_API_TOKEN,
   },
